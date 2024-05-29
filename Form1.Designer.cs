68b835a03aadb41a8769a8b1b850c1ff2e58f8d3
Douglas Morales <DouGMoralesVelasquez@hotmail.com>
2024-05-28 23:45:08 -0600

    Se cambio la clase abastecimiento, se agrego datos.json y se cambio el manejo de datos

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,181 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonAbastecer = new System.Windows.Forms.Button();
+            this.textBoxCliente = new System.Windows.Forms.TextBox();
+            this.labelNombreCliente = new System.Windows.Forms.Label();
+            this.labelMonto = new System.Windows.Forms.Label();
+            this.buttonGenerarEstadisticas = new System.Windows.Forms.Button();
+            this.comboBoxBomba = new System.Windows.Forms.ComboBox();
+            this.labelBomba = new System.Windows.Forms.Label();
+            this.buttonMostrarAbastecimiento = new System.Windows.Forms.Button();
+            this.textBoxMontoPagar = new System.Windows.Forms.TextBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.comboBoxTipoAbastecimiento = new System.Windows.Forms.ComboBox();
+            this.labelMetodoAbastecimiento = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonAbastecer
+            // 
+            this.buttonAbastecer.Location = new System.Drawing.Point(266, 268);
+            this.buttonAbastecer.Name = "buttonAbastecer";
+            this.buttonAbastecer.Size = new System.Drawing.Size(105, 30);
+            this.buttonAbastecer.TabIndex = 0;
+            this.buttonAbastecer.Text = "Abastecer";
+            this.buttonAbastecer.UseVisualStyleBackColor = true;
+            this.buttonAbastecer.Click += new System.EventHandler(this.buttonAbastecer_Click);
+            // 
+            // textBoxCliente
+            // 
+            this.textBoxCliente.Location = new System.Drawing.Point(238, 127);
+            this.textBoxCliente.Name = "textBoxCliente";
+            this.textBoxCliente.Size = new System.Drawing.Size(146, 22);
+            this.textBoxCliente.TabIndex = 1;
+            // 
+            // labelNombreCliente
+            // 
+            this.labelNombreCliente.AutoSize = true;
+            this.labelNombreCliente.Location = new System.Drawing.Point(80, 127);
+            this.labelNombreCliente.Name = "labelNombreCliente";
+            this.labelNombreCliente.Size = new System.Drawing.Size(123, 16);
+            this.labelNombreCliente.TabIndex = 4;
+            this.labelNombreCliente.Text = "Nombre del cliente:";
+            // 
+            // labelMonto
+            // 
+            this.labelMonto.AutoSize = true;
+            this.labelMonto.Location = new System.Drawing.Point(80, 172);
+            this.labelMonto.Name = "labelMonto";
+            this.labelMonto.Size = new System.Drawing.Size(97, 16);
+            this.labelMonto.TabIndex = 5;
+            this.labelMonto.Text = "Monto a pagar:";
+            // 
+            // buttonGenerarEstadisticas
+            // 
+            this.buttonGenerarEstadisticas.Location = new System.Drawing.Point(0, 0);
+            this.buttonGenerarEstadisticas.Name = "buttonGenerarEstadisticas";
+            this.buttonGenerarEstadisticas.Size = new System.Drawing.Size(75, 23);
+            this.buttonGenerarEstadisticas.TabIndex = 11;
+            // 
+            // comboBoxBomba
+            // 
+            this.comboBoxBomba.FormattingEnabled = true;
+            this.comboBoxBomba.Items.AddRange(new object[] {
+            "Bomba 1",
+            "Bomba 2"});
+            this.comboBoxBomba.Location = new System.Drawing.Point(238, 80);
+            this.comboBoxBomba.Name = "comboBoxBomba";
+            this.comboBoxBomba.Size = new System.Drawing.Size(146, 24);
+            this.comboBoxBomba.TabIndex = 7;
+            // 
+            // labelBomba
+            // 
+            this.labelBomba.AutoSize = true;
+            this.labelBomba.Location = new System.Drawing.Point(80, 80);
+            this.labelBomba.Name = "labelBomba";
+            this.labelBomba.Size = new System.Drawing.Size(54, 16);
+            this.labelBomba.TabIndex = 8;
+            this.labelBomba.Text = "Bomba:";
+            // 
+            // buttonMostrarAbastecimiento
+            // 
+            this.buttonMostrarAbastecimiento.Location = new System.Drawing.Point(256, 334);
+            this.buttonMostrarAbastecimiento.Name = "buttonMostrarAbastecimiento";
+            this.buttonMostrarAbastecimiento.Size = new System.Drawing.Size(125, 50);
+            this.buttonMostrarAbastecimiento.TabIndex = 9;
+            this.buttonMostrarAbastecimiento.Text = "Abastecimientos Previos";
+            this.buttonMostrarAbastecimiento.UseVisualStyleBackColor = true;
+            this.buttonMostrarAbastecimiento.Click += new System.EventHandler(this.buttonMostrarAbastecimiento_Click);
+            // 
+            // textBoxMontoPagar
+            // 
+            this.textBoxMontoPagar.Location = new System.Drawing.Point(238, 172);
+            this.textBoxMontoPagar.Name = "textBoxMontoPagar";
+            this.textBoxMontoPagar.Size = new System.Drawing.Size(146, 22);
+            this.textBoxMontoPagar.TabIndex = 10;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(433, 44);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(240, 197);
+            this.dataGridView.TabIndex = 12;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(433, 272);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(240, 150);
+            this.richTextBox1.TabIndex = 13;
+            this.richTextBox1.Text = "";
+            // 
+            // comboBoxTipoAbastecimiento
+            // 
+            this.comboBoxTipoAbastecimiento.FormattingEnabled = true;
+            this.comboBoxTipoAbastecimiento.Items.AddRange(new object[] {
+            "Prepago limitado",
+            "Tanque lleno"});
+            this.comboBoxTipoAbastecimiento.Location = new System.Drawing.Point(238, 217);
+            this.comboBoxTipoAbastecimiento.Name = "comboBoxTipoAbastecimiento";
+            this.comboBoxTipoAbastecimiento.Size = new System.Drawing.Size(146, 24);
+            this.comboBoxTipoAbastecimiento.TabIndex = 14;
+            // 
+            // labelMetodoAbastecimiento
+            // 
+            this.labelMetodoAbastecimiento.AutoSize = true;
+            this.labelMetodoAbastecimiento.Location = new System.Drawing.Point(80, 220);
+            this.labelMetodoAbastecimiento.Name = "labelMetodoAbastecimiento";
+            this.labelMetodoAbastecimiento.Size = new System.Drawing.Size(152, 16);
+            this.labelMetodoAbastecimiento.TabIndex = 15;
+            this.labelMetodoAbastecimiento.Text = "Tipo de abastecimiento:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(748, 450);
+            this.Controls.Add(this.labelMetodoAbastecimiento);
+            this.Controls.Add(this.comboBoxTipoAbastecimiento);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.textBoxMontoPagar);
+            this.Controls.Add(this.buttonMostrarAbastecimiento);
+            this.Controls.Add(this.labelBomba);
+            this.Controls.Add(this.comboBoxBomba);
+            this.Controls.Add(this.buttonGenerarEstadisticas);
+            this.Controls.Add(this.labelMonto);
+            this.Controls.Add(this.labelNombreCliente);
+            this.Controls.Add(this.textBoxCliente);
+            this.Controls.Add(this.buttonAbastecer);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonAbastecer;
+        private System.Windows.Forms.TextBox textBoxCliente;
+        private System.Windows.Forms.Label labelNombreCliente;
+        private System.Windows.Forms.Label labelMonto;
+        private System.Windows.Forms.Button buttonGenerarEstadisticas;
+        private System.Windows.Forms.ComboBox comboBoxBomba;
+        private System.Windows.Forms.Label labelBomba;
+        private System.Windows.Forms.Button buttonMostrarAbastecimiento;
+        private System.Windows.Forms.TextBox textBoxMontoPagar;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ComboBox comboBoxTipoAbastecimiento;
+        private System.Windows.Forms.Label labelMetodoAbastecimiento;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,150 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace ProyectoFinalProgra
 {
     public partial class Form1 : Form
     {
+        private SerialPort serialPort;
+        List<Abastecimiento> abastecimientos = new List<Abastecimiento>();
+
         public Form1()
         {
             InitializeComponent();
+            serialPort = new SerialPort("COM3", 9600);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!serialPort.IsOpen)
+                {
+                    serialPort.Open();
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("Acceso denegado al puerto COM. Asegúrate de que no esté siendo utilizado por otra aplicación.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+        }
+
+        private void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = serialPort.ReadLine();
+            if (data.Contains("OK"))
+            {
+                MessageBox.Show("Datos recibidos correctamente por el Arduino.", "Confirmación", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        public void MostrarAbastecimientos()
+        {
+            //Mostrar la lista de Reporte en el DataGridView
+            dataGridView.DataSource = null;
+            dataGridView.DataSource = abastecimientos;
+            dataGridView.Refresh();
+        }
+
+        private void buttonAbastecer_Click(object sender, EventArgs e)
+        {
+            // Obtener datos
+            abastecimientos.Clear();
+            Abastecimiento abastecimiento = new Abastecimiento();
+            abastecimiento.Bomba = comboBoxBomba.Text;
+            abastecimiento.Cliente = textBoxCliente.Text;
+            abastecimiento.FechaHora = DateTime.Now;
+            abastecimiento.Monto = textBoxMontoPagar.Text;
+            abastecimiento.TipoAbasatecimiento = comboBoxTipoAbastecimiento.Text;
+
+            // Guardar datos
+            abastecimientos.Add(abastecimiento);
+            MostrarAbastecimientos();
+            GrabarAbastecimientos();
+            GuardarDatosComoJson();
+
+            textBoxCliente.Clear();
+            textBoxMontoPagar.Clear();
+        }
+
+        private void GuardarDatosComoJson()
+        {
+            string json = JsonConvert.SerializeObject(abastecimientos, Formatting.Indented);
+            File.WriteAllText("datos.json", json);
+
+            EnviarDatosArduino(json);
+        }
+
+        private void EnviarDatosArduino(string datos)
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.Write(datos);
+            }
+            else
+            {
+                MessageBox.Show("El puerto COM no está abierto.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void GrabarAbastecimientos()
+        {
+            FileStream stream = new FileStream("Abastecimientos.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(stream);
+
+            foreach (var abastecimiento in abastecimientos)
+            {
+                writer.WriteLine(abastecimiento.Cliente);
+                writer.WriteLine(abastecimiento.FechaHora);
+                writer.WriteLine(abastecimiento.Bomba);
+                writer.WriteLine(abastecimiento.Monto);
+                writer.WriteLine(abastecimiento.TipoAbasatecimiento);
+            }
+            writer.Close();
+        }
+
+        private void buttonMostrarAbastecimiento_Click(object sender, EventArgs e)
+        {
+            // Ruta de archivo
+            string filePath = "C:\\Users\\Sir_d\\source\\repos\\ProyectoFinalProgra\\bin\\Debug\\abastecimientos.txt";
+
+            if (File.Exists(filePath))
+            {
+                FileStream flujo = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+                StreamReader lector = new StreamReader(flujo);
+
+                // Limpiar el contenido previo del richTextBox
+                richTextBox1.Clear();
+
+                // Leer y mostrar el contenido del archivo en el richTextBox
+                while (lector.Peek() > -1)
+                {
+                    richTextBox1.AppendText(lector.ReadLine() + "\n");
+                }
+                lector.Close();
+            }
+            else
+            {
+                MessageBox.Show("El archivo especificado no existe.");
+            }
         }
     }
 }
